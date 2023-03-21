383727a068820f11651b2b704e83d047a51d6bbb
Otabek <otabekrahmonovtest@gmail.com>
2023-03-21 23:12:13 +0500

    feat: BookList

--- apps/education/views.py
+++ apps/education/views.py
@@ -1,4 +1,6 @@
+from rest_framework import filters
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Book
 from .serializers import BookListSerializer
@@ -7,3 +9,6 @@ from .serializers import BookListSerializer
 class BookListAPIView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookListSerializer
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ('title',) 
+    filterset_fields = ('leval_id', 'author_id', 'language_id')
